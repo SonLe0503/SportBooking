d04944a5c2c5f83f36925b762137306d409dbf33
SonLe0503 <SonLVHE172736@fpt.edu.vn>
2025-10-18 09:22:43 +0700

    Initial commit

---
+++ SportBooking/Models/Booking.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SportBooking.Models;
+
+public partial class Booking
+{
+    public int BookingId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public int? FieldId { get; set; }
+
+    public DateTime? BookingDate { get; set; }
+
+    public TimeOnly? StartTime { get; set; }
+
+    public TimeOnly? EndTime { get; set; }
+
+    public decimal? TotalPrice { get; set; }
+
+    public string? Status { get; set; }
+
+    public virtual Field? Field { get; set; }
+
+    public virtual ICollection<Payment> Payments { get; set; } = new List<Payment>();
+
+    public virtual User? User { get; set; }
+}
---
+++ SportBooking/Models/SportBookingDbContext.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportBooking.Models;
+
+public partial class SportBookingDbContext : DbContext
+{
+    public SportBookingDbContext()
+    {
+    }
+
+    public SportBookingDbContext(DbContextOptions<SportBookingDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Booking> Bookings { get; set; }
+
+    public virtual DbSet<Feedback> Feedbacks { get; set; }
+
+    public virtual DbSet<Field> Fields { get; set; }
+
+    public virtual DbSet<ImageField> ImageFields { get; set; }
+
+    public virtual DbSet<Payment> Payments { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Booking>(entity =>
+        {
+            entity.HasKey(e => e.BookingId).HasName("PK__Bookings__C6D03BED0688CCEF");
+
+            entity.Property(e => e.BookingId).HasColumnName("bookingID");
+            entity.Property(e => e.BookingDate)
+                .HasColumnType("datetime")
+                .HasColumnName("bookingDate");
+            entity.Property(e => e.EndTime).HasColumnName("endTime");
+            entity.Property(e => e.FieldId).HasColumnName("fieldID");
+            entity.Property(e => e.StartTime).HasColumnName("startTime");
+            entity.Property(e => e.Status)
+                .HasMaxLength(50)
+                .HasDefaultValue("Pending")
+                .HasColumnName("status");
+            entity.Property(e => e.TotalPrice)
+                .HasColumnType("decimal(10, 2)")
+                .HasColumnName("totalPrice");
+            entity.Property(e => e.UserId).HasColumnName("userID");
+
+            entity.HasOne(d => d.Field).WithMany(p => p.Bookings)
+                .HasForeignKey(d => d.FieldId)
+                .HasConstraintName("FK__Bookings__fieldI__412EB0B6");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Bookings)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK__Bookings__userID__403A8C7D");
+        });
+
+        modelBuilder.Entity<Feedback>(entity =>
+        {
+            entity.HasKey(e => e.FeedbackId).HasName("PK__Feedback__2613FDC4EC80AAA4");
+
+            entity.Property(e => e.FeedbackId).HasColumnName("feedbackID");
+            entity.Property(e => e.Comment).HasColumnName("comment");
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("createdAt");
+            entity.Property(e => e.FieldId).HasColumnName("fieldID");
+            entity.Property(e => e.Rating).HasColumnName("rating");
+            entity.Property(e => e.UserId).HasColumnName("userID");
+
+            entity.HasOne(d => d.Field).WithMany(p => p.Feedbacks)
+                .HasForeignKey(d => d.FieldId)
+                .HasConstraintName("FK__Feedbacks__field__46E78A0C");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Feedbacks)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK__Feedbacks__userI__45F365D3");
+        });
+
+        modelBuilder.Entity<Field>(entity =>
+        {
+            entity.HasKey(e => e.FieldId).HasName("PK__Fields__F0AC27FE11A5C9C1");
+
+            entity.Property(e => e.FieldId).HasColumnName("fieldID");
+            entity.Property(e => e.Description).HasColumnName("description");
+            entity.Property(e => e.FieldName)
+                .HasMaxLength(100)
+                .HasColumnName("fieldName");
+            entity.Property(e => e.Image)
+                .HasMaxLength(255)
+                .HasColumnName("image");
+            entity.Property(e => e.Location)
+                .HasMaxLength(255)
+                .HasColumnName("location");
+            entity.Property(e => e.OwnerId).HasColumnName("ownerID");
+            entity.Property(e => e.Price)
+                .HasColumnType("decimal(10, 2)")
+                .HasColumnName("price");
+
+            entity.HasOne(d => d.Owner).WithMany(p => p.Fields)
+                .HasForeignKey(d => d.OwnerId)
+                .OnDelete(DeleteBehavior.SetNull)
+                .HasConstraintName("FK__Fields__ownerID__3C69FB99");
+        });
+
+        modelBuilder.Entity<ImageField>(entity =>
+        {
+            entity.HasKey(e => e.ImageId).HasName("PK__ImageFie__336E9B757CB1FE15");
+
+            entity.ToTable("ImageField");
+
+            entity.Property(e => e.ImageId).HasColumnName("imageID");
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("createdAt");
+            entity.Property(e => e.FieldId).HasColumnName("fieldID");
+            entity.Property(e => e.ImageUrl)
+                .HasMaxLength(255)
+                .HasColumnName("imageUrl");
+
+            entity.HasOne(d => d.Field).WithMany(p => p.ImageFields)
+                .HasForeignKey(d => d.FieldId)
+                .HasConstraintName("FK__ImageFiel__field__59FA5E80");
+        });
+
+        modelBuilder.Entity<Payment>(entity =>
+        {
+            entity.HasKey(e => e.PaymentId).HasName("PK__Payments__A0D9EFA69735E67D");
+
+            entity.Property(e => e.PaymentId).HasColumnName("paymentID");
+            entity.Property(e => e.Amount)
+                .HasColumnType("decimal(10, 2)")
+                .HasColumnName("amount");
+            entity.Property(e => e.BookingId).HasColumnName("bookingID");
+            entity.Property(e => e.PaymentDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("paymentDate");
+            entity.Property(e => e.PaymentMethod)
+                .HasMaxLength(50)
+                .HasColumnName("paymentMethod");
+            entity.Property(e => e.Status)
+                .HasMaxLength(50)
+                .HasDefaultValue("Completed")
+                .HasColumnName("status");
+
+            entity.HasOne(d => d.Booking).WithMany(p => p.Payments)
+                .HasForeignKey(d => d.BookingId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK__Payments__bookin__4BAC3F29");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__Users__CB9A1CDF96F8506E");
+
+            entity.HasIndex(e => e.Username, "UQ__Users__F3DBC572E4679EEF").IsUnique();
+
+            entity.Property(e => e.UserId).HasColumnName("userID");
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("createdAt");
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .HasColumnName("email");
+            entity.Property(e => e.Password)
+                .HasMaxLength(255)
+                .HasColumnName("password");
+            entity.Property(e => e.Phone)
+                .HasMaxLength(15)
+                .HasColumnName("phone");
+            entity.Property(e => e.Role)
+                .HasMaxLength(20)
+                .HasDefaultValue("USER")
+                .HasColumnName("role");
+            entity.Property(e => e.Username)
+                .HasMaxLength(50)
+                .HasColumnName("username");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
